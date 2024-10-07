a9ccfce18a262951d998534710119a1044f960cd
TimMannn <150806544+TimMannn@users.noreply.github.com>
2024-10-07 15:26:18 +0200

    React + API? toegevoegd

---
+++ Web api/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new List<string> { "Item1", "Item2" };
+            return Ok(data);
+        }
+    }
+}
