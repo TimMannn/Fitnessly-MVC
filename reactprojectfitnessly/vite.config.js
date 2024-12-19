00e3df142fef6a95cd100cadfdb7317f9e691157
TimMannn <150806544+TimMannn@users.noreply.github.com>
2024-12-19 01:30:43 +0100

    Vite.config.js bijgewerkt. Certificates en nginx ook toegevoegd.

--- reactprojectfitnessly/vite.config.js
+++ reactprojectfitnessly/vite.config.js
@@ -3,7 +3,31 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import child_process from 'child_process';
 import { env } from 'process';
+
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
+
+const certificateName = "ReactProjectFitnessly";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+if (process.env.CI !== 'true' && (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath))) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit', }).status) {
+        console.warn("Could not create certificate. Proceeding with HTTP.");
+    }
+}
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
@@ -17,7 +41,16 @@ export default defineConfig({
         }
     },
     server: {
+        proxy: {
+            '^/weatherforecast': {
+                target,
+                secure: false
+            }
+        },
         port: 3000,
-        https: false
+        https: process.env.CI === 'true' ? false : {
+            key: fs.existsSync(keyFilePath) ? fs.readFileSync(keyFilePath) : undefined,
+            cert: fs.existsSync(certFilePath) ? fs.readFileSync(certFilePath) : undefined,
+        }
     }
 });
